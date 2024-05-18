1e8a063ad5a742605e4aba93ab31dc693ba5851c
Abdullah Salman <abdullah.salman@devsinc.com>
2024-05-19 00:17:21 +0500

    async part1 done

---
+++ async.js
@@ -0,0 +1,41 @@
+// js is syncrounous
+// it is single thread.is
+
+// one code is executed one time
+
+// todo ExecutionContext
+//  1. call stack
+//  2. Memory heap
+
+
+// todo Blocking Code vs Non Blocking Code
+
+// todo Blocking
+//  block the flow of the program  wait()
+// eg: read file sync (kernel jab tak file read na kar le tab tak baqi block rehta)
+
+
+// todo Non Blocking
+// baqi ke kam b hoty rahin gay our file b read hoti rhy gi
+// todo register callback is used ( task queue) kam hony ke baad humin yaad karwa de kam ho gaya
+// task queue se data call stack ma jata ha our execute ho jata ha
+
+
+console.log("2")
+setTimeout(() => {
+  console.log("4")
+
+}, timeout);
+
+console.log("3")
+
+// out put will be 2,3,4
+
+// because 4 wali line register callback se taskqueue ma jay gi waha se
+// call stack ma jay gi
+
+
+// for fetch task queue is high priority
+
+
+// ?Part 2
