e52fd1f6f2cb82121b109b0dc614282f595ecea1
Abdullah Salman <abdullah.salman@devsinc.com>
2024-05-19 13:23:55 +0500

    interface tuple done

---
+++ typescript/interface.ts
@@ -0,0 +1,9 @@
+interface UserA {
+  name: string,
+  age: number
+}
+
+
+// it is like a class
+
+const abdullah: UserA = {name: 'ab', age:1}
---
+++ typescript/tuple.ts
@@ -0,0 +1,7 @@
+const user :(string| number)[] = [1, "abc"];
+
+
+// todo if we want to make sure the order of the array presists then we use it
+
+let rgb : [number, number, number] = [1, 1, 1]
+// todo strict comparison only 3 values of numbers are allowed.
